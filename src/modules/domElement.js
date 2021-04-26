1c88c4d4b90bd6186a7c46a05a1cfce560dbe6f6
Smogo <aurelien.genois@gmail.com>
2021-04-26 15:46:16 +0200

    lint on other files

--- src/modules/domElement.js
+++ src/modules/domElement.js
@@ -1,35 +1,32 @@
-
 const createDomElements = ((doc) => {
-    const createParagraph = (textContent) => {
-        const paragraph = doc.createElement('p');
-        paragraph.textContent = textContent;
-        return paragraph;
+  const createParagraph = (textContent) => {
+    const paragraph = doc.createElement('p');
+    paragraph.textContent = textContent;
+    return paragraph;
+  };
+  const createImg = (ImgSrc) => {
+    const img = doc.createElement('img');
+    img.src = ImgSrc;
+    return img;
+  };
+  const createInput = (type) => {
+    let input;
+    if (type === 'textarea') {
+      input = doc.createElement('textarea');
+    } else {
+      input = doc.createElement('input');
+      input.type = type;
     }
-    const createImg = (ImgSrc) => {
-        const img = doc.createElement('img');
-        img.src = ImgSrc;
-        return img;
-    }
-    const createInput =  (type) => {
-        let input;
-        if(type === 'textarea') {
-            input = doc.createElement('textarea');
-        } else {
-            input = doc.createElement('input');
-            input.type = type;
-        }
-        return input;
-    }
-    const createLabel = (textContent, forAttr) => {
-        const label = doc.createElement('label');
-        label.htmlFor = forAttr;
-        label.textContent = textContent;
-        return label;
-    }
-    
-    return {createParagraph, createImg, createInput, createLabel};
-})(document)
+    return input;
+  };
+  const createLabel = (textContent, forAttr) => {
+    const label = doc.createElement('label');
+    label.htmlFor = forAttr;
+    label.textContent = textContent;
+    return label;
+  };
 
-export {
-    createDomElements
-}
+  return { createParagraph, createImg, createInput, createLabel };
+})(document);
+
+export { createDomElements };
